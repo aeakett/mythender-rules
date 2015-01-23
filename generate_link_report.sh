7219bf66099796b98fc09176c7574914fb031401
Andrew Eakett <aeakett@gmail.com>
2015-01-23 12:12:03 -0500

    add script to generate link report

---
+++ generate_link_report.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+curl http://validator.w3.org/checklink\?uri=http%3A%2F%2Feakett.ca%2Fmythender%2F\&summary=on\&hide_type=all\&recursive=on\&depth=1\&cookie=set\&check=Check > link_report.htm
