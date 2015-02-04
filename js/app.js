6cf1f4226e24c8da7d1be0b73e5cb6e4d17cfdb3
Andrew Eakett <aeakett@gmail.com>
2015-02-04 11:58:37 -0500

    new navigation

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation();
+$(document).foundation({
+   offcanvas : {
+      // Sets method in which offcanvas opens.
+      // [ move | overlap_single | overlap ]
+      open_method: 'overlap_single'
+   }
+});
+
+$(document).ready(function(){
+   $('.navList .toggle').click(function(){
+      $(this).next().slideToggle();
+      if ($(this).text() == '+') {
+         $(this).text('-');
+      } else {
+         $(this).text('+');
+      }
+   });
+});
