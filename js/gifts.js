e58812474c8ac7fc5713814e7d6485f71de0d24a
Andrew Eakett <aeakett@gmail.com>
2015-02-06 09:38:10 -0500

    close menu when changing which gifts to view

--- js/gifts.js
+++ js/gifts.js
@@ -2,14 +2,17 @@ $('input[name="show"]').change(function(){
    switch ($(this).attr('value')) {
       case 'all':
          $('.enderOnly, .mythOnly').show();
+			$('.exit-off-canvas').click();
          break;
       case 'eo':
          $('.enderOnly').show();
          $('.mythOnly').hide();
+			$('.exit-off-canvas').click();
          break;
       case 'mo':
          $('.mythOnly').show();
          $('.enderOnly').hide();
+			$('.exit-off-canvas').click();
          break;
    }
 });
