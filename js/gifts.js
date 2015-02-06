4b59e352c3e5cfceeb989d7c271aded56aa4bb91
Andrew Eakett <aeakett@gmail.com>
2015-02-06 11:46:39 -0500

    add "checkboxes" for gift selector

--- js/gifts.js
+++ js/gifts.js
@@ -1,22 +1,22 @@
 $('#showAllGifts').click(function(){
    $('.enderOnly, .mythOnly').show();
-	$('#showMythenderGifts, #showMythGifts').removeClass('giftSelected');
-	$(this).addClass('giftSelected');
+	$('#showMythenderGifts, #showMythGifts').removeClass('giftSelected').addClass('giftUnselected');
+	$(this).removeClass('giftUnselected').addClass('giftSelected');
    $('.exit-off-canvas').click();
 });
 
 $('#showMythenderGifts').click(function(){
    $('.enderOnly').show();
    $('.mythOnly').hide();
-	$('#showAllGifts, #showMythGifts').removeClass('giftSelected');
-	$(this).addClass('giftSelected');
+	$('#showAllGifts, #showMythGifts').removeClass('giftSelected').addClass('giftUnselected');
+	$(this).removeClass('giftUnselected').addClass('giftSelected');
    $('.exit-off-canvas').click();
 });
 
 $('#showMythGifts').click(function(){
    $('.mythOnly').show();
    $('.enderOnly').hide();
-   $('#showAllGifts, #showMythenderGifts').removeClass('giftSelected');
-	$(this).addClass('giftSelected');
+   $('#showAllGifts, #showMythenderGifts').removeClass('giftSelected').addClass('giftUnselected');
+	$(this).removeClass('giftUnselected').addClass('giftSelected');
    $('.exit-off-canvas').click();
 });
