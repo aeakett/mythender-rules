733692d263de4c9218457eedcb054170dc372916
Andrew Eakett <aeakett@gmail.com>
2016-05-03 11:18:24 -0400

    convert to jekyll

--- js/app.js
+++ js/app.js
@@ -1,20 +1,4 @@
-// Foundation JavaScript
-// Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation({
-   offcanvas : {
-      // Sets method in which offcanvas opens.
-      // [ move | overlap_single | overlap ]
-      open_method: 'overlap_single'
-   }
-});
-
-$(document).ready(function(){
-   $('.navList .toggle').click(function(){
-      $(this).next().slideToggle();
-      if ($(this).text() == '+') {
-         $(this).text('–');
-      } else {
-         $(this).text('+');
-      }
-   });
+$(document).foundation();
+$(function() {
+   FastClick.attach(document.body);
 });
